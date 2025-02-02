f6c6d8100160e00b4976bc57279f3a29e1a9ece6
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2025-02-02 16:27:18 +0600

    [API] I changed the server logic so that when the first client connects, it sees an empty canvas.

--- backend/index.ts
+++ backend/index.ts
@@ -1,41 +1,54 @@
 import express from 'express';
-import http from 'http';
-import WebSocket from 'ws';
+import expressWs from 'express-ws';
+import cors from 'cors';
+import {WebSocket} from 'ws';
 
 const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-let pixelData: Array<{ x: number, y: number }> = [];
-
-
-const updatePixelData = (x: number, y: number) => {
-    pixelData.push({ x, y });
-};
-
-wss.on('connection', (ws: WebSocket) => {
-    console.log('Новый клиент подключился');
-
-    ws.send(JSON.stringify(pixelData));
-
-    ws.on('message', (message: any) => {
-        const data = JSON.parse(message);
-        updatePixelData(data.x, data.y);
-        const updatedPixelData = JSON.stringify(pixelData);
-
-        wss.clients.forEach((client: WebSocket) => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(updatedPixelData); // Отправляем полный список пикселей
-            }
-        });
-    });
-
-    ws.on('close', () => {
-        console.log('Клиент отключился');
-    });
-});
+expressWs(app);
 
 const port = 8000;
-server.listen(port, () => {
-    console.log(`Сервер запущен на http://localhost:${port}`);
+app.use(cors());
+
+
+const router = express.Router();
+const connectionClients: WebSocket[] = [];
+let pixelData: Array<{ x: number, y: number }> = [];
+
+router.ws('/canvas',  (ws, req) => {
+    connectionClients.push(ws);
+    console.log('client connected! Client total - ', connectionClients.length);
+    if(connectionClients.length === 1){
+       pixelData = [];
+    }
+        ws.send(JSON.stringify(pixelData));
+
+        ws.on('message', (message: any) => {
+            try{
+                const data = JSON.parse(message);
+                pixelData.push({ x: data.x, y: data.y });
+
+                const updatedPixelData = JSON.stringify(pixelData);
+
+                connectionClients.forEach((client: WebSocket) => {
+                    if (client !== ws && client.readyState === WebSocket.OPEN) {
+                        client.send(updatedPixelData);
+                    }
+                });
+            }
+            catch (e) {
+                ws.send(JSON.stringify({error: "Invalid message format"}));
+            }
+        });
+
+    ws.on('close', () => {
+        console.log('client disconnected!');
+        const index = connectionClients.indexOf(ws);
+        connectionClients.splice(index, 1);
+    });
+    });
+
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`Server started on ${port} port!`);
 });
